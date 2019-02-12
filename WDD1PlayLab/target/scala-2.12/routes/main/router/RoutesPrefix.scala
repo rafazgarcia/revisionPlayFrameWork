70dc84e17da2ec7f63197afdd13f07ac8b306354
rafazgarcia <dj_rafael_garcia@hotmail.com>
2019-02-12 14:14:12 +0000

    STEP1

---
+++ WDD1PlayLab/target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/wdd/revisionPlayFrameWork/WDD1PlayLab/conf/routes
+// @DATE:Tue Feb 12 11:44:40 GMT 2019
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
